b9d79cb2051a3871ee56a0b253e7f8a876d39473
Camilla Romagnoli <romagnolicamilla@gmail.com>
2025-01-05 17:09:56 -0300

    feat: configure firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:my_recipes/config/dependencies.dart';
 import 'package:my_recipes/core/design/colors.dart';
 import 'package:my_recipes/core/routes/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   configureDependencies();
 
   runApp(const MyApp());
